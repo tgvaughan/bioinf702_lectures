a028511955caf1fad817696a5dfab8dab834d9e8
alexeid <alexei.drummond@gmail.com>
2017-03-22 12:43:49 +1300

    wright fisher in processing js

--- lecture10/wrightFisher.R
+++ lecture10/wrightFisher.R
@@ -1,4 +1,4 @@
-plotWrightFisher <- function(N=12, G=21,sorted=T, unsorted=T, pch=1, seed=NA) {
+plotWrightFisher <- function(N=12, G=21,sorted=T, unsorted=T, pch=1, seed=NA, lines=T, plotNodeLabels=F, col="gray", horizontal=F) {
   
   if (!is.na(seed)) {
   	set.seed(seed);
@@ -7,13 +7,31 @@ plotWrightFisher <- function(N=12, G=21,sorted=T, unsorted=T, pch=1, seed=NA) {
   x <- rep(1:N, times=G); 
   y <- rep(0:(G-1), each=N)
   p <- sample(1:N,N*(G-1),replace=T)
-  par(mar = c(3,5,1,1) + 0.1, mfrow=c(1,ifelse(sorted && unsorted, 2, 1)))
+  mar <- c(3,5,1,1) + 0.1;
+  if (sorted && unsorted) {
+  	par(mar = mar, mfrow=ifelse(horizontal,c(2,1),c(1,2)))
+  } else {
+  	par(mar = mar)
+  }
   
-  plot(x,y,xlab="", ylab="Generations in the past", xaxt="n")
-  mtext("individuals", side=1, line=1)
+  xlab <- "individuals";
+  ylab <- "Generations in the past";
+  if (horizontal) {
+  	tmp <- xlab;
+  	xlab <- ylab;
+  	ylab <- tmp;
+  }
+  
+  if (horizontal) {
+    plot(unlist(ifelse(horizontal,list(y),list(x))),unlist(ifelse(horizontal,list(x),list(y))),xlab="", ylab=ylab, yaxt="n", col=col)
+  } else {
+    plot(unlist(ifelse(horizontal,list(y),list(x))),unlist(ifelse(horizontal,list(x),list(y))),xlab="", ylab=ylab, xaxt="n", col=col)
+  }    
+
+  mtext(xlab, side=1, line=1)
 
   if (unsorted) {
-  	lx <- c(rbind(x[1:length(p)],p,NA))
+    lx <- c(rbind(x[1:length(p)],p,NA))
     ly <- c(rbind(y,y+1,NA))
     lines(lx[1:(3*N*(G-1))],ly[1:(3*N*(G-1))],col="black")
     points(x,y,pch=16,col="white")
@@ -22,8 +40,9 @@ plotWrightFisher <- function(N=12, G=21,sorted=T, unsorted=T, pch=1, seed=NA) {
 
   if (sorted) {
     if (unsorted) {
-      plot(x,y,xlab="", ylab="Generations in the past", xaxt="n")
-      mtext("individuals", side=1, line=1)
+      plot(unlist(ifelse(horizontal,list(y),list(x))),unlist(ifelse(horizontal,list(x),list(y))),xlab="", ylab=ylab, 
+        xaxt=ifelse(horizontal,"","n"), yaxt=ifelse(horizontal,"n",""), col=col)
+      mtext(xlab, side=1, line=1)
     }
     
     # the labels of the x after sorting
@@ -42,15 +61,85 @@ plotWrightFisher <- function(N=12, G=21,sorted=T, unsorted=T, pch=1, seed=NA) {
       np <- c(match(parents[xl[1:N]],xl_parents), np);  
     }
 
-    lx <- c(rbind(x[1:length(p)],np,NA))
-    ly <- c(rbind(y,y+1,NA))
-    lines(lx[1:(3*N*(G-1))],ly[1:(3*N*(G-1))],col="black")
+    if (lines) {
+      lx <- c(rbind(x[1:length(p)],np,NA))
+      ly <- c(rbind(y,y+1,NA))
+      
+      if (horizontal) {
+      	tmp <- lx;
+      	lx <- ly;
+      	ly <- tmp;
+      }
+      
+      lines(lx[1:(3*N*(G-1))],ly[1:(3*N*(G-1))], col=col)
+    }
+    
+      if (horizontal) {
+      	tmp <- x;
+      	x <- y;
+      	y <- tmp;
+      }
+
     points(x,y,pch=16,col="white")
-    points(x,y,pch=pch)
-    #text(x+0.05,y-0.05,xl)
+    points(x,y,pch=pch, col=col)
+    if (plotNodeLabels) {
+    	text(x,y,xl,cex=0.6)
+    }
     #text(x,y+0.5,np, col="red")
   }  
   
-  result <- list(xl=xl, np=np, p=p)
+  result <- list(N=N, G=G, node_labels=xl, parent_positions=np, x=x, y=y)
   return (result)
+}
+
+# takes a vector of node labels and plots their ancestry back to the oldest generation
+plotAncestralLineages <- function(wrightFisher, node_labels, pch=16, col="red", plotRootLineage=F, horizontal=F) {
+
+	x <- c();
+	y <- c();
+	lx <- c();
+	ly <- c();
+	
+	N <- wrightFisher$N
+	G <- wrightFisher$G
+
+	node_positions <- match(node_labels, wrightFisher$node_labels[1:N])
+
+	i <- 0;
+
+	while  (i < G & (length(node_positions) > 1 | plotRootLineage)) {
+			
+		x <- c(x, node_positions);
+		y <- c(y, rep(i, length(node_positions)));
+		
+		start <- i*N+1;
+
+		parent_positions <- wrightFisher$parent_positions[start:(start+N)]
+		parent_positions <- parent_positions[node_positions]
+		
+		lx <- c(lx, rbind(node_positions,parent_positions,NA))
+		ly <- c(ly, rbind(rep(i, length(node_positions)),rep(i+1, length(node_positions)),NA))
+		
+		node_positions <- unique(parent_positions);
+		i = i + 1;
+	}
+	
+	if (length(node_positions) == 1 & !plotRootLineage) {
+		x <- c(x, node_positions);
+		y <- c(y, i);
+	}
+	
+	if (horizontal) {
+      		tmp <- x;
+      		x <- y;
+      		y <- tmp;
+
+      		tmp <- lx;
+      		lx <- ly;
+      		ly <- tmp;
+
+	}
+
+    	lines(lx,ly,col=col)
+	points(x, y, pch=pch, col=col);	
 }
---
+++ lecture10/wrightFisher.js
@@ -0,0 +1,157 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight); 
+  background(255);  
+  
+  N = 32;
+  G = 64;
+  
+  pop = initialPopulation(G, N);
+  
+  sortPopulation(pop);
+} 
+
+function draw() {
+  
+  stroke(127,127,127);
+  strokeWeight(0.5);
+  plotWrightFisher();
+  
+  stroke(255,0,0);
+  strokeWeight(1.5);
+  traceCoalescence([1,2,3,4,5,6],pop[0]);
+}
+
+function mouseClicked() {
+  background(255);  
+  pop = initialPopulation(G, N);
+  sortPopulation(pop);	
+}
+
+function plotWrightFisher() {
+
+  scale = min(width/(N+2),height/(G+2));
+  
+  xMargin = (width - scale*(N+2)) / 2
+  yMargin = (height - scale*(G+2)) / 2
+  
+  pointSize = 7;
+  
+  for (i = 1; i <= N; i++) {
+    for (j = 1; j <= G; j++) {
+      
+      x = i*scale+xMargin
+      y = height - yMargin - j*scale
+
+      plotParentLine(pop[j-1][i-1])
+    }
+  }
+  
+  for (i = 1; i <= N; i++) {
+    for (j = 1; j <= G; j++) {
+      x = i*scale+xMargin
+      y = height - yMargin - j*scale
+
+      //text(pop[j-1][i-1].label,i*scale+xMargin, height - yMargin - j*scale)
+      ellipse(x, y, pointSize, pointSize)
+    }
+  }
+}
+
+function plotParentLine(node) {
+  x = (node.num+1)*scale+xMargin;
+  y = height - yMargin - (node.gen+1)*scale;
+
+  if (node["parent"]) {
+    p = node.parent;
+    px = (p.num+1)*scale+xMargin;
+    py = height - yMargin - (p.gen+1)*scale;
+
+    line(x,y,px,py);
+  }
+}
+
+function plotCoalescentLine(node) {
+  x1 = scale+xMargin;
+  x2 = scale*N+xMargin;
+  y = height - yMargin - (node.gen+1)*scale;
+  
+  line(x1,y,x2,y);
+}
+
+function initialPopulation(G, N){
+
+  var arr = [];
+
+  // Creates all generations:
+  for(var i=0; i < G; i++){
+
+      // Creates an empty line
+      arr.push([]);
+
+      // Adds cols to the empty line:
+      arr[i].push( new Array(N));
+  }
+  
+  // populate generations from oldest to most recent
+  for(var i=G-1; i >= 0; i--){
+
+      for(var j=0; j < N; j++){
+        // Initializes:
+        arr[i][j] = {label:j+1};
+		arr[i][j].gen = i;
+        arr[i][j].num = j;
+        if (i<G-1) {
+          arr[i][j].parent = arr[i+1][Math.floor((Math.random() * N))];
+        }
+      }
+  }
+
+  return arr;
+}
+
+function traceCoalescence(labels, gen) {
+
+  nodes = [];
+  
+  for(var j=0; j < N; j++) {
+    if (labels.indexOf(gen[j].label) > -1) {
+      nodes.push(gen[j]);
+    }
+  }
+  traceNodes(nodes);
+}
+  
+function traceNodes(nodes) {
+  
+  parents = [];
+
+  for(var j=0; j < nodes.length; j++){
+    plotParentLine(nodes[j]);
+    if (nodes[j]["parent"]) {
+      if (parents.indexOf(nodes[j].parent) === -1) {
+        parents.push(nodes[j].parent);
+      }
+    }
+  }
+  
+  if (parents.length < nodes.length && parents.length > 0) {
+    stroke(0,0,255);
+    plotCoalescentLine(parents[0]);	
+    stroke(255,0,0);
+  }
+  
+  if (parents.length > 0) { traceNodes(parents); }
+}
+
+function sortPopulation(pop) {
+
+  for (i = (G-2); i >= 0; i--) {
+    gen = pop[i];
+    // sort this generation by parent numbers
+    gen.sort(function(a, b) { return a.parent.num - b.parent.num });
+    // renumber this generation after sorting
+    for(var j=0; j < N; j++){
+      gen[j].num = j;
+    }
+  }  
+}
