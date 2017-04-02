56de148db383d13781bba33208d9ad69d754b3b6
Tim Vaughan <tgvaughan@gmail.com>
2017-04-02 17:49:47 +1200

    Completed master equation slides of L13.

---
+++ lecture13/SIR_demo.js
@@ -0,0 +1,97 @@
+var SIR = (function() {
+    var pub = {};
+
+    pub.N = 100;
+    pub.beta = 0.01;
+    pub.mu = 0.1;
+
+    function Tinf(S,I,R) {
+        return pub.beta*S*I;
+    }
+
+    function Trem(S,I,R) {
+        return pub.mu*I;
+    }
+
+    pub.getStochastic = function (T) {
+        var S = [pub.N-1], I=[1], R=[0], t=[0];
+
+        var i = 0;
+        while (t[i]<T) {
+            var Ti = Tinf(S[i], I[i], R[i]);
+            var Tr = Trem(S[i], I[i], R[i]);
+            var T0 = Ti + Tr;
+
+            if (T0 == 0)
+                break;
+
+            var dt = -Math.log(Math.random())/T0;
+            if (t[i] + dt > T)
+                break;
+
+            t.push(t[i] + dt);
+
+            if (Math.random()*T0 < Ti) {
+                // Infection
+                S.push(S[i]-1);
+                I.push(I[i]+1);
+                R.push(R[i]);
+            } else {
+                // Recovery
+                S.push(S[i]);
+                I.push(I[i]-1);
+                R.push(R[i]+1);
+            }
+
+            i += 1;
+        }
+
+        S.push(S[i]);
+        I.push(I[i]);
+        R.push(R[i]);
+        t.push(T);
+
+        return {
+            S: S,
+            I: I,
+            R: R,
+            t: t
+        };
+    }
+
+    pub.getDeterministic = function (T, nSteps) {
+
+        var dt = T/(nSteps - 1);
+
+        S = [pub.N-1], I=[1], R=[0], t=[0];
+
+        for (var i=1; i<nSteps; i++) {
+
+            var Sp = S[i-1], Ip = I[i-1], Rp = R[i-1]; 
+
+            for (var iter=0; iter<3; iter++) {
+                var Spp = S[i-1] - Tinf(Sp,Ip,Rp)*0.5*dt
+                var Ipp = I[i-1] + (Tinf(Sp,Ip,Rp) - Trem(Sp,Ip,Rp))*0.5*dt
+                var Rpp = R[i-1] + Trem(Sp,Ip,Rp)*0.5*dt
+
+                Sp = Spp, Ip = Ipp, Rp = Rpp;
+            }
+
+            S.push(Sp*2 - S[i-1]);
+            I.push(Ip*2 - I[i-1]);
+            R.push(Rp*2 - R[i-1]);
+            t.push(t[i-1] + dt);
+        }
+
+        return {
+            S: S,
+            I: I,
+            R: R,
+            t: t
+        };
+    }
+
+    return pub;
+})();
+
+
